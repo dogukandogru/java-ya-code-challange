d048ced750acead593cdcabce25e0be1f80fad17
Dogukan <dogukandogruu@gmail.com>
2022-09-23 14:15:17 +0300

    gets earthquake data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.SQLOutput;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -20,6 +23,39 @@ class Country{
     private double minLatitude;
     private double minLongitude;
     private double maxLatitude;
+
+    public double getMinLatitude() {
+        return minLatitude;
+    }
+
+    public void setMinLatitude(double minLatitude) {
+        this.minLatitude = minLatitude;
+    }
+
+    public double getMinLongitude() {
+        return minLongitude;
+    }
+
+    public void setMinLongitude(double minLongitude) {
+        this.minLongitude = minLongitude;
+    }
+
+    public double getMaxLatitude() {
+        return maxLatitude;
+    }
+
+    public void setMaxLatitude(double maxLatitude) {
+        this.maxLatitude = maxLatitude;
+    }
+
+    public double getMaxLongitude() {
+        return maxLongitude;
+    }
+
+    public void setMaxLongitude(double maxLongitude) {
+        this.maxLongitude = maxLongitude;
+    }
+
     private double maxLongitude;
 
     public Country(double minLatitude, double minLongitude, double maxLatitude, double maxLongitude) {
@@ -37,52 +73,55 @@ public class Main {
     private static final String googleGeocodingBaseURL = "https://maps.googleapis.com/maps/api/geocode/json";
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        /*String input = scanner.nextLine();
-        String country = input.split(", ")[0];
-        String countOfDays = input.split(", ")[1];
-         */
+        String input = scanner.nextLine();
+        int countOfDays = Integer.parseInt(input.split(", ")[1]);
 
-        String url = baseURL;
 
         try{
-            getCoordinate("Turkey");
+            Country country = getCountry(input.split(", ")[0]);
+            String starttime = getDate(countOfDays);
+            String urlString = baseURL + "&minlatitude=" + country.getMinLatitude() + "&maxlatitude=" + country.getMaxLatitude()
+                    + "&minlongitude=" + country.getMinLongitude() + "&maxlongitude=" + country.getMaxLongitude() + "&starttime=" + starttime;
+
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int status = con.getResponseCode();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            con.disconnect();
+
+            System.out.println(content);
+        }
+        catch (MalformedURLException e){
+            System.out.println(e);
+        }
+        catch (IOException e){
+            System.out.println(e);
         }
         catch (Exception e){
             System.out.println(e);
         }
-
-        /*
-        try{
-            URL url = new URL();
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-
-        }
-        catch (MalformedURLException e){
-
-        }
-        catch (IOException e){
-
-        }
-        */
     }
 
     public static String addParameter(String parameter, String value){
         return baseURL  + "&" + parameter + "=" + value;
     }
 
-    private static Country getCoordinate(String country) throws IOException {
+    private static Country getCountry(String country) throws IOException {
         String urlString = googleGeocodingBaseURL + "?address=" + country + "&key=" +  googleGeocodingAPIKey;
-        System.out.println(urlString);
         URL url = new URL(urlString);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
 
         int status = con.getResponseCode();
-        System.out.println(status);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer content = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
@@ -105,9 +144,12 @@ public class Main {
         double minLongitude = Math.min(northeast.getDouble("lng"), southwest.getDouble("lng"));
         double maxLongitude = Math.max(northeast.getDouble("lng"), southwest.getDouble("lng"));
 
-        System.out.println(minLatitude + " " + maxLatitude  + " " + minLongitude +" "  + maxLongitude);
-
         return new Country(minLatitude, minLongitude, maxLatitude, maxLongitude);
+    }
+
+    private static String getDate(int countOfDays){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return localDateTime.minusDays(countOfDays).format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 
 }
